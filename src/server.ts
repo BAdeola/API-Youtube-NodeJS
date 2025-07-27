96089b12f9361a362876c91a04009e5a8176180f
Brayan Adeolá <brayanadeola@gmail.com>
2025-07-27 16:46:12 -0300

    feat: starting server with environment variables ---

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Server is running on port 3000");
+import * as http from 'http';
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  
+});
+const port = process.env.PORT; //doc .env
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
